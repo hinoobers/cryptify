39f99a7644f658ea347d60c2729eb6839d20f5a4
reiop <redisolenmina1@gmail.com>
2024-08-31 01:03:41 +0300

    Add functionality

--- scripts/encrypt.js
+++ scripts/encrypt.js
@@ -1,6 +1,14 @@
 function encrypt() {
-    var text = document.getElementById("text").value;
+    var text = document.getElementById("input").value;
     var algorithm = document.getElementById("algorithm").value;
+
+    var encrypted = "";
+    switch(algorithm) {
+        case "base64":
+            console.log("Encrypting with base64");
+            encrypted = btoa(text);
+            break;
+    }
 
     console.log("Encrypting text: " + text + " with algorithm: " + algorithm);
 }
