4dc50126f79dac28f8456614f844c060b88c2dfb
reiop <redisolenmina1@gmail.com>
2024-08-31 01:01:27 +0300

    Create html & start of encryption

---
+++ scripts/encrypt.js
@@ -0,0 +1,6 @@
+function encrypt() {
+    var text = document.getElementById("text").value;
+    var algorithm = document.getElementById("algorithm").value;
+
+    console.log("Encrypting text: " + text + " with algorithm: " + algorithm);
+}
